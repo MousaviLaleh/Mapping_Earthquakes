bab5f26438d5927385c9d75971097b0a8761b5c7
MousaviLaleh <laleh.moosavy@gmail.com>
2022-07-27 19:19:38 -0400

    update code

---
+++ Simple_Map/static/js/config.js
@@ -0,0 +1,5 @@
+
+// Mapbox API Key
+// https://www.mapbox.com/pricing/?utm_medium=blog&utm_source=mapbox-blog&utm_campaign=blog%7Cmapbox-blog%7Cpricing%7Cnew-pricing-46b7c26166e7-19-05&utm_term=pricing&utm_content=new-pricing-46b7c26166e7
+
+const API_KEY = 'pk.eyJ1IjoibGFsZWhtb29zYXZ5IiwiYSI6ImNsNjJiem05NjBhMzczaHA1MGRrb29yN3AifQ.fK0xJ92di5VPVD7_YfxxfA';
